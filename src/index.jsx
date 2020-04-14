1ccd97cb6e026d474490c32834ef471ed2447772
nexuslifeline <nexuslifeline@gmail.com>
2020-04-14 16:16:18 +0800

    comment out other modules

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-export { default as Icon } from './components/Icon/Icon';
-export { default as Packery } from './components/Packery/Packery';
+//export { default as Icon } from './components/Icon/Icon';
+//export { default as Packery } from './components/Packery/Packery';
 export { default as Form } from './components/Form/Form';
-export {
-  request,
-  authRequest,
-  toCamelCase,
-  toCamelCaseDict,
-  checkStatus
-}
-from './helpers/apis';
+// export {
+//   request,
+//   authRequest,
+//   toCamelCase,
+//   toCamelCaseDict,
+//   checkStatus
+// }
+// from './helpers/apis';
